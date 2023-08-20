779a1ab5ef97fe85da91ebe140a0c964ecc61dc4
salma <gsalmahossain@gmail.com>
2023-08-20 16:13:53 +0200

    create a rest contoller

--- src/main/java/io/javabrains/springbootquickstart/CourseApi.java
+++ src/main/java/io/javabrains/springbootquickstart/CourseApi.java
@@ -1,7 +1,11 @@
 package io.javabrains.springbootquickstart;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CourseApi {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(CourseApi.class, args);
     }
 }
