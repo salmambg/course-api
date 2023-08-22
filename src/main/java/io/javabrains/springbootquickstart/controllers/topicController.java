5856796469158e9064b209d431f2de45490a8aec
salma <gsalmahossain@gmail.com>
2023-08-22 11:10:05 +0200

    create a resourse using PUT request

--- src/main/java/io/javabrains/springbootquickstart/controllers/topicController.java
+++ src/main/java/io/javabrains/springbootquickstart/controllers/topicController.java
@@ -24,6 +24,10 @@ public class topicController {
     @RequestMapping(method = RequestMethod.POST, value = "/topics")
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic,@PathVariable String id) {
+        topicService.updateTopic(id ,topic);
 
     }
 }
--- src/main/java/io/javabrains/springbootquickstart/controllers/topicService.java
+++ src/main/java/io/javabrains/springbootquickstart/controllers/topicService.java
@@ -23,5 +23,14 @@ import java.util.List;
         public void addTopic(Topic topic) {
             topics.add(topic);
         }
+        public void updateTopic(String id, Topic topic) {
+            for (int i = 0; i < topics.size(); i++) {
+                Topic t = topics.get(i);
+                if (t.getId().equals(id)) {
+                    topics.set(i, topic);
+                    return;
+                }
+            }
+        }
 
 }
