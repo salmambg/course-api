9e9285ae25f7ef59645d2d3a3bac8b8da6dbab1e
salma <gsalmahossain@gmail.com>
2023-08-22 10:41:40 +0200

    creating post API

--- src/main/java/io/javabrains/springbootquickstart/controllers/topicController.java
+++ src/main/java/io/javabrains/springbootquickstart/controllers/topicController.java
@@ -1,20 +1,29 @@
 package io.javabrains.springbootquickstart.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController // whatever you return is get automatically converted to json and sent back as the http response
 public class topicController {
+    @Autowired
+    private topicService topicService;
 
     @RequestMapping("/topics")
     public List<Topic>getAllTopics() {
-        return Arrays.asList(
-                new Topic("Spring", "Spring Framework","Spring FrameWork Description"),
-                new Topic("Java", "JavaCore Framework","Java Description"),
-                new Topic("Javascript", "JavaScript Framework","JavaScript Description")
-        );
+        return topicService.getAllTopics();
+
+    }
+    @RequestMapping("/topics/{id}") //this is a path
+    public Topic getTopic(@PathVariable String id) { //by making curly braces and put a token as a variable part for Spring MVC
+        // now spring to send that variable in the argument of the method , we have to use another annotation path variable to get the variable from the path
+        return topicService.getTopic(id);
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+
     }
 }
---
+++ src/main/java/io/javabrains/springbootquickstart/controllers/topicService.java
@@ -0,0 +1,27 @@
+package io.javabrains.springbootquickstart.controllers;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+    @Service
+    public class topicService {
+        private List<Topic> topics = new ArrayList<>(Arrays.asList(
+                new Topic("Spring", "Spring Framework","Spring FrameWork Description"),
+                new Topic("Java", "JavaCore Framework","Java Description"),
+                new Topic("Javascript", "JavaScript Framework","JavaScript Description")
+        ));
+        public List<Topic> getAllTopics() {
+            return topics;
+        }
+        public Topic getTopic(String id) {
+            return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        }
+        public void addTopic(Topic topic) {
+            topics.add(topic);
+        }
+
+}
